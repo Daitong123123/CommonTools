a317aaf9d3d01ed9619d6becd642bdfdd909f190
d30055011 <daitong3@huawei.com>
2025-04-22 18:35:21 +0800

    新增模型切换功能

--- tools/src/main/java/com/daitong/controller/ModelConfigController.java
+++ tools/src/main/java/com/daitong/controller/ModelConfigController.java
@@ -3,14 +3,11 @@ package com.daitong.controller;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.daitong.bo.aichat.UnlikeRequest;
 import com.daitong.bo.common.BasePageResponse;
 import com.daitong.bo.common.BaseResponse;
 import com.daitong.bo.common.CommonResponse;
 import com.daitong.bo.common.PageRequest;
-import com.daitong.manager.UserManager;
 import com.daitong.repository.ModelConfigRepository;
-import com.daitong.repository.entity.DishDisappear;
 import com.daitong.repository.entity.ModelConfig;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,30 +31,30 @@ public class ModelConfigController {
     @GetMapping("/admin/model-type-list")
     public BaseResponse modelTypeList() {
         BaseResponse baseResponse = new BaseResponse();
-        try{
+        try {
             baseResponse.setCode("200");
             baseResponse.setMessage("请求成功");
             List<String> modelTypeList = modelConfigRepository.selectObjs("model_type").stream().map(String::valueOf).collect(Collectors.toList());
             baseResponse.setData(modelTypeList);
             return baseResponse;
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("请求失败", e);
-        baseResponse.setCode("500");
-        baseResponse.setMessage(e.getMessage());
+            baseResponse.setCode("500");
+            baseResponse.setMessage(e.getMessage());
         }
         return baseResponse;
     }
 
-    @GetMapping("/admin/model-list")
-    public BaseResponse modelTypeList(String modelType) {
+    @GetMapping("/admin/model-list-by-type")
+    public BaseResponse modelListByType(String modelType) {
         BaseResponse baseResponse = new BaseResponse();
-        try{
+        try {
             baseResponse.setCode("200");
             baseResponse.setMessage("请求成功");
             List<ModelConfig> list = modelConfigRepository.list(new QueryWrapper<ModelConfig>().lambda().eq(ModelConfig::getModelType, modelType));
             baseResponse.setData(list);
             return baseResponse;
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("请求失败", e);
             baseResponse.setCode("500");
             baseResponse.setMessage(e.getMessage());
@@ -68,13 +65,13 @@ public class ModelConfigController {
     @GetMapping("/admin/model-current")
     public BaseResponse modelCurrent() {
         BaseResponse baseResponse = new BaseResponse();
-        try{
+        try {
             baseResponse.setCode("200");
             baseResponse.setMessage("请求成功");
             ModelConfig modelConfig = modelConfigRepository.getOne(new QueryWrapper<ModelConfig>().lambda().eq(ModelConfig::isSelected, true));
             baseResponse.setData(modelConfig);
             return baseResponse;
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("请求失败", e);
             baseResponse.setCode("500");
             baseResponse.setMessage(e.getMessage());
@@ -85,7 +82,7 @@ public class ModelConfigController {
     @GetMapping("/admin/switch-model")
     public BaseResponse modelSwitch(@RequestParam String modelType, @RequestParam String model) {
         BaseResponse baseResponse = new BaseResponse();
-        try{
+        try {
             baseResponse.setCode("200");
             baseResponse.setMessage("请求成功");
             ModelConfig oldConfig = modelConfigRepository.getOne(new LambdaQueryWrapper<ModelConfig>().eq(ModelConfig::isSelected, true));
@@ -95,7 +92,7 @@ public class ModelConfigController {
             config.setSelected(true);
             modelConfigRepository.save(config);
             return baseResponse;
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("请求失败", e);
             baseResponse.setCode("500");
             baseResponse.setMessage(e.getMessage());
@@ -106,7 +103,7 @@ public class ModelConfigController {
     @PostMapping("/admin/add-model")
     public BaseResponse modelAdd(@RequestBody ModelConfig config) {
         BaseResponse baseResponse = new BaseResponse();
-        try{
+        try {
             baseResponse.setCode("200");
             baseResponse.setMessage("请求成功");
             config.setSelected(false);
@@ -114,7 +111,7 @@ public class ModelConfigController {
             config.setUpdatedAt(new Date());
             modelConfigRepository.save(config);
             return baseResponse;
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("请求失败", e);
             baseResponse.setCode("500");
             baseResponse.setMessage(e.getMessage());
@@ -125,14 +122,14 @@ public class ModelConfigController {
     @PostMapping("/admin/update-model")
     public BaseResponse modelUpdate(@RequestBody ModelConfig config) {
         BaseResponse baseResponse = new BaseResponse();
-        try{
+        try {
             baseResponse.setCode("200");
             baseResponse.setMessage("请求成功");
             config.setUpdatedAt(new Date());
             config.setUpdatedAt(new Date());
             modelConfigRepository.saveOrUpdate(config);
             return baseResponse;
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("请求失败", e);
             baseResponse.setCode("500");
             baseResponse.setMessage(e.getMessage());
@@ -143,15 +140,15 @@ public class ModelConfigController {
     @PostMapping("/admin/model-list")
     public BasePageResponse modelList(@RequestBody PageRequest pageRequest) {
         BasePageResponse basePageResponse = new BasePageResponse();
-        try{
+        try {
             basePageResponse.setCode("200");
             basePageResponse.setMessage("请求成功");
-           Page<ModelConfig> page = new Page<>(pageRequest.getCurPage(), pageRequest.getPageSize());
-           modelConfigRepository.page(page);
-           basePageResponse.setTotal((int) page.getTotal());
-           basePageResponse.setData(page.getRecords());
+            Page<ModelConfig> page = new Page<>(pageRequest.getCurPage(), pageRequest.getPageSize());
+            modelConfigRepository.page(page);
+            basePageResponse.setTotal((int) page.getTotal());
+            basePageResponse.setData(page.getRecords());
             return basePageResponse;
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("请求失败", e);
             basePageResponse.setCode("500");
             basePageResponse.setMessage(e.getMessage());
@@ -162,12 +159,12 @@ public class ModelConfigController {
     @PostMapping("/admin/delete-model")
     public CommonResponse modelDelete(@RequestBody List<Long> deleteList) {
         BaseResponse baseResponse = new BaseResponse();
-        try{
+        try {
             baseResponse.setCode("200");
             baseResponse.setMessage("请求成功");
             modelConfigRepository.removeByIds(deleteList);
             return baseResponse;
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("请求失败", e);
             baseResponse.setCode("500");
             baseResponse.setMessage(e.getMessage());
