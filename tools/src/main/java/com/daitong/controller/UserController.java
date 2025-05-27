273f74176ec7fb4ec32c9e9ea5205d5e5ca22d1d
d30055011 <daitong3@huawei.com>
2025-05-27 10:14:57 +0800

    增加更换头像功能

--- tools/src/main/java/com/daitong/controller/UserController.java
+++ tools/src/main/java/com/daitong/controller/UserController.java
@@ -3,9 +3,9 @@ package com.daitong.controller;
 import com.daitong.bo.common.BaseResponse;
 import com.daitong.manager.UserManager;
 import com.daitong.repository.UserRepository;
+import com.daitong.repository.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,6 +19,21 @@ public class UserController {
         BaseResponse baseResponse = new BaseResponse();
         try {
             baseResponse.setData(userRepository.updateIconId(UserManager.getCurrentUser().getUserId(), iconId));
+            baseResponse.setCode("200");
+            baseResponse.setMessage("上传成功");
+            return baseResponse;
+        } catch (Exception e) {
+            baseResponse.setCode("500");
+            return baseResponse;
+        }
+    }
+
+    @GetMapping("/userinfo")
+    public BaseResponse getUserInfo(String userId) {
+        BaseResponse baseResponse = new BaseResponse();
+        try {
+            UserEntity userInfo = userRepository.getUserInfo(userId);
+            baseResponse.setData(userInfo);
             baseResponse.setCode("200");
             baseResponse.setMessage("上传成功");
             return baseResponse;
